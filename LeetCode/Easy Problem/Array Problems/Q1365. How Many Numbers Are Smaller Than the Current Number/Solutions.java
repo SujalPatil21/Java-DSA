755943ec7c0d00fe92c49940a781f657f1367119
Sujal Patil <sujalbpatil21@gmail.com>
2026-01-27 23:46:26 +0530

    Fix nested loop and return statement in solution

--- LeetCode/Easy Problem/Array Problems/Q1365. How Many Numbers Are Smaller Than the Current Number/Solutions.java
+++ LeetCode/Easy Problem/Array Problems/Q1365. How Many Numbers Are Smaller Than the Current Number/Solutions.java
@@ -5,16 +5,17 @@ class Solution {
 
         for(int i = 0 ; i < nums.length ; i++){
             int count = 0;
-            for(int j = 0 ; j<nums.length; j++){
+                for(int j = 0 ; j<nums.length; j++){
 
 
-                if(nums[i] > nums[j] ){
+                    if(nums[i] > nums[j] ){
 
-                    count++;
+                        count++;
+                    }
+                    ans[i] = count;
+
                 }
-                ans[i] = count;
-
             }
-            return ans;
+                return ans;
         }
     }
