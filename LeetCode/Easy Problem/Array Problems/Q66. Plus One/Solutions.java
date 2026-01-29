470f83d8a041ac6b0685ed0dcf2371a41d116f06
SujalPatil21 <sujalbpatil21@gmail.com>
2026-01-29 19:25:05 +0530

    Q66. Plus One

---
+++ LeetCode/Easy Problem/Array Problems/Q66. Plus One/Solutions.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+
+
+        int[] ans = new int[digits.length+1];
+
+        for(int i = digits.length-1 ; i >=0 ; i--){
+
+            if(digits[i] < 9){
+
+                digits[i] += 1;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        ans[0] =1 ;
+        return ans;
+    }
+}
