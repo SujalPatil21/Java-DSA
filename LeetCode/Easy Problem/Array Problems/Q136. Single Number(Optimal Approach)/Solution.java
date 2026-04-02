725ab267f649999daa6730a175cce0bfb40fa69d
SujalPatil21 <sujalbpatil21@gmail.com>
2026-04-02 11:58:22 +0530

    Add solution and documentation for Q136. Single Number(Optimal Approach)

---
+++ LeetCode/Easy Problem/Array Problems/Q136. Single Number(Optimal Approach)/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+
+        int Xor = 0;
+
+        for(int n : nums){
+
+            Xor ^= n;
+        }
+
+        return Xor;
+        
+    }
+}
